7a91be557ffa130dbf7e2b89eecd8eb68389fe61
Surya221299 <60531747+Surya221299@users.noreply.github.com>
2023-05-03 21:36:35 +0700

    Add files via upload

---
+++ Swift basic-Auto Layout Calculator/Swift basic-Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift basic-Auto Layout Calculator
+//
+//  Created by Bamantara S on 03/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
